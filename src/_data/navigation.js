67933ec595759a1c04cc0e026256dc580d7c5c23
Rohan Paranjpe <rap2363@gmail.com>
2025-07-15 10:57:19 -0700

    changes

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -2,6 +2,6 @@ module.exports = [
     { url: "/", title: "Home", order: 1 },
     { url: "/contact/", title: "Contact", order: 2 },
     { url: "/services/", title: "Services", order: 3 },
-    { url: "https://substack.com/@rap2363", title: "Blog", order: 4 },
+    { url: "https://substack.com/@rap2363", title: "Blog", target: "_blank", order: 4 },
     { url: "/about/", title: "About", order: 5 },
 ];
